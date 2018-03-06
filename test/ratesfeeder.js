073564a345b14a16a4f562243b7cb0fbc00f68f7
KP <github@kolczapeter.hu>
2018-03-06 14:49:09 +0100

    const insted of var/let

--- test/ratesfeeder.js
+++ test/ratesfeeder.js
@@ -1,21 +1,21 @@
-var assert = require('assert');
-let ratesFeeder = require('../src/RatesFeeder.js');
+const assert = require('assert');
+const ratesFeeder = require('../src/RatesFeeder.js');
 
 describe('RatesFeeder: real exchange rate tests', function () {
 
     it('Kraken interface should return a number', async function () {
-        let price = await ratesFeeder.getKrakenPrice("EUR");
+        const price = await ratesFeeder.getKrakenPrice("EUR");
         assert.equal('number', typeof price);
     });
 
     it('BitStamp interface should return a number', async function () {
-        let price = await ratesFeeder.getBitstampPrice("EUR");
+        const price = await ratesFeeder.getBitstampPrice("EUR");
         assert.equal('number', typeof price);
     });
 
 
     it('set on-chain rate and should be the same', async function () {
-        let price = await ratesFeeder.getPrice("EUR");
+        const price = await ratesFeeder.getPrice("EUR");
         await ratesFeeder.updatePrice("EUR");
         const storedRate = await ratesFeeder.augmintRatesInstance.rates("EUR");
         assert.equal( parseInt(price *ratesFeeder.decimalsDiv),storedRate[0].c[0]);
