e85c0ebeb06406ac37b149aa1d34d8dce7d1ac88
KP <github@kolczapeter.hu>
2018-03-06 10:19:54 +0100

    basic tests for feeder

---
+++ test/ratesfeeder.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+let ratesFeeder = require('../src/RatesFeeder.js');
+
+describe('RatesFeeder: real exchange rate tests', function () {
+
+    it('Kraken interface should return a number', async function () {
+        let price = await ratesFeeder.getKrakenPrice("EUR");
+        assert.equal('number', typeof price);
+    });
+
+    it('BitStamp interface should return a number', async function () {
+        let price = await ratesFeeder.getBitstampPrice("EUR");
+        assert.equal('number', typeof price);
+    });
+
+
+    it('set on-chain rate and should be the same', async function () {
+        let price = await ratesFeeder.getPrice("EUR");
+        await ratesFeeder.updatePrice("EUR");
+        const storedRate = await ratesFeeder.augmintRatesInstance.rates("EUR");
+        assert.equal( parseInt(price *ratesFeeder.decimalsDiv),storedRate[0].c[0]);
+    });
+
+});
