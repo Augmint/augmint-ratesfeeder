268afbe4b5fd78e50aa86e1c36ea57257ec9e42e
Peter Petrovics <szerintedmi@gmail.com>
2018-03-07 17:52:21 +0000

    actual first, expected  after in assert.equal

--- test/ratesfeeder.js
+++ test/ratesfeeder.js
@@ -8,17 +8,17 @@ describe("RatesFeeder: real exchange rate tests", function() {
 
     it("Kraken interface should return a number", async function() {
         const price = await ratesFeeder.getKrakenPrice("EUR");
-        assert.equal("number", typeof price);
+        assert.equal(typeof price, "number");
     });
 
     it.skip("BitStamp interface should return a number", async function() {
         const price = await ratesFeeder.getBitstampPrice("EUR");
-        assert.equal("number", typeof price);
+        assert.equal(typeof price, "number");
     });
 
     it("Gdax interface should return a number", async function() {
         const price = await ratesFeeder.getGdaxPrice("EUR");
-        assert.equal("number", typeof price);
+        assert.equal(typeof price, "number");
     });
 
     it("set on-chain rate and should be the same", async function() {
@@ -26,6 +26,6 @@ describe("RatesFeeder: real exchange rate tests", function() {
         await ratesFeeder.updatePrice("EUR");
         // TODO: check tx.logs[0].event + args ?
         const storedRate = await ratesFeeder.augmintRatesInstance.rates("EUR");
-        assert.equal(parseInt(price * ratesFeeder.decimalsDiv), storedRate[0].c[0]);
+        assert.equal(storedRate[0].c[0], parseInt(price * ratesFeeder.decimalsDiv));
     });
 });
