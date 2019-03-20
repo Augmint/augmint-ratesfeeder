702dc29c27f00baa8eec9b4be11ec49f18f360d5
szerintedmi <szerintedmi@gmail.com>
2019-03-20 11:54:40 +0800

    gdax provider test

--- test/rateProviders.js
+++ test/rateProviders.js
@@ -1,13 +1,63 @@
 /* TODO: Integration test with real rates provider feeds */
-//const assert = require("assert");
-// const baseHelpers = require("./helpers/base.js");
+const chai = require("chai");
+const assert = chai.assert;
+const sinon = require("sinon");
 
-describe.skip("rateProviders: test interfaces", function() {
-    before(async function() {});
+const TickerProvider = require("src/tickerProviders/TickerProvider.js");
 
-    it("Kraken interface should return a number", async function() {});
+const gdaxTickerProvider = require("src/tickerProviders/gdaxTickerProvider.js");
+let ticker;
 
-    it.skip("BitStamp interface should return a number", async function() {});
+describe("GDAX ticker provider tests", () => {
+    before(() => {
+        ticker = new TickerProvider(gdaxTickerProvider.definition);
+    });
 
-    it("Gdax interface should return a number", async function() {});
+    it("should have correct state before connect", () => {
+        const status = ticker.getStatus();
+        assert.equal(status.name, "GDAX");
+
+        assert(!status.isConnected);
+        assert.isNull(status.connectedAt);
+        assert.isNull(status.lastHeartbeat);
+        assert.isNull(status.reconnectCount);
+
+        assert.isNull(status.lastTicker);
+    });
+
+    it("should connect and have initial ticker", async () => {
+        // how to avoid ticker update triggering right after connect when websocket/pusher connected?
+        const connectedSpy = sinon.spy();
+        ticker.on("connected", connectedSpy);
+
+        await ticker.connectAndSubscribe();
+        assert(connectedSpy.calledOnce);
+
+        const connectionTime = new Date();
+        let status = ticker.getStatus();
+
+        assert.equal(status.name, "GDAX");
+        assert(status.isConnected);
+        assert(Math.abs(status.connectedAt - connectionTime < 3));
+        assert(Math.abs(status.lastHeartbeat - connectionTime) < 3);
+        assert.equal(status.reconnectCount, 0);
+
+        assert.isNumber(status.lastTicker.price);
+        assert(Math.abs(status.lastTicker.time - connectionTime) < 5);
+
+        const disconnectedSpy = sinon.spy();
+        const disconnectingSpy = sinon.spy();
+        ticker.on("disconnecting", disconnectingSpy);
+        ticker.on("disconnected", disconnectedSpy);
+
+        await ticker.disconnect();
+        assert(disconnectedSpy.calledOnce);
+        assert(disconnectingSpy.calledOnce);
+        status = ticker.getStatus();
+        assert(!status.isConnected);
+    });
+
+    it("should terminate for SIGINT");
+
+    it("should return ticker after ticker updated"); // how to mock ticker update?
 });
