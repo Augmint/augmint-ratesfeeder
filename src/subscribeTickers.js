614f0aa7b5a9a2f6d49c1ceb0df44a7c61ec7fd9
szerintedmi <szerintedmi@gmail.com>
2019-03-22 17:35:20 +0000

    module name capitalisation fix

--- src/subscribeTickers.js
+++ src/subscribeTickers.js
@@ -1,6 +1,6 @@
 const CoinbaseTickerProvider = require("src/tickerProviders/CoinbaseHttpTicker.js");
 const KrakenTickerProvider = require("src/tickerProviders/KrakenHttpTicker.js");
-const BitstampTickerProvider = require("src/tickerProviders/BitStampHttpTicker.js");
+const BitstampTickerProvider = require("src/tickerProviders/BitstampHttpTicker.js");
 
 const tickers = [
     new CoinbaseTickerProvider(), //
