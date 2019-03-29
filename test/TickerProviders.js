8f9d3c1d6da46a456485632ec4295ee8d23a993a
szerintedmi <szerintedmi@gmail.com>
2019-03-29 18:23:04 +0000

    fix timestamp assertion in test

--- test/TickerProviders.js
+++ test/TickerProviders.js
@@ -157,7 +157,7 @@ tickerProviders.forEach(Provider => {
             assert.isAtMost(status.lastPollAttemptAt - startedAt, 2000);
             assert.isAtMost(status.lastTicker.requestedAt - status.lastPollAttemptAt, 500);
             assert.isAtMost(status.lastTicker.receivedAt - status.lastPollAttemptAt, 10000);
-            assert.isNotEmpty(status.lastTicker.time);
+            assert.instanceOf(status.lastTicker.time, Date);
 
             assert.isNumber(status.lastTicker.price);
 
