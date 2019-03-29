a699f137cde11b9170cb8a2d673b214fd0cfc9b1
szerintedmi <szerintedmi@gmail.com>
2019-03-29 18:20:25 +0000

    remove .only

--- test/TickerProviders.js
+++ test/TickerProviders.js
@@ -258,7 +258,7 @@ tickerProviders.forEach(Provider => {
             assert(tickerPriceChangedSpy.calledOnce);
         });
 
-        it.only("Should only update ticker if time is newer", async () => {
+        it("Should only update ticker if time is newer", async () => {
             const scope = nock(MOCKS[tickerClassName].host)
                 // after connect
                 .get(MOCKS[tickerClassName].path)
